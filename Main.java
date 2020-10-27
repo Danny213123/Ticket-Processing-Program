f0227bd1020e9eea5a945057f74e5992f188d5ae
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2020-10-27 19:15:49 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        ReadFile Scan = new ReadFile(3, 1);
+        
+        String[][] StringArray = Scan.ScanFile();
+        
+        for (int x = 0; x < StringArray.length; x++){
+            for (int q = 0; q < StringArray[0].length; q++){
+                System.out.println(StringArray[x][q]);
+            }
+        }
+        
+        int NumberOfCustomers = Scan.GetSize();
+        int TotalNumberOfTickets = Scan.GetInputs();
+        
+        CustomerInformation[] Customers = new CustomerInformation[StringArray.length];
+        
+        for (int y = 0; y < Customers.length; y ++){
+            Customers[y] = new CustomerInformation(Integer.parseInt(StringArray[y][1]), Integer.parseInt(StringArray[y][2]), StringArray[y][0]);
+        }
+        
+        WriteFile Output = new WriteFile();
+        Output.OutputFile(Customers);
+        
+    }
+}
