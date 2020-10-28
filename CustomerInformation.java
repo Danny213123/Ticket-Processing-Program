96e6ac67d4a9eacc8654e164bd4f5339fb17e915
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2020-10-28 11:47:16 -0400

    Update CustomerInformation.java

--- CustomerInformation.java
+++ CustomerInformation.java
@@ -1,27 +1,59 @@
+/** 
+ * Organizes customer information
+ *
+ * @Customer information Class
+ * @Author - Danny Guan
+ * @Version - 1
+ *
+ */
+
 public class CustomerInformation {
     private int StandardTickets;
     private int VIPTickets;
     private String Email;
     
+    /** CustomerInformation Constructor.
+     *  
+     * @Param int StandardTickets - Number of standard ticket  the customer wants.
+     * @Param int VIPTickets - Number of VIP tickets the customer wants.
+     * @Param String Email - Email of the customer.
+     * 
+     */
     public CustomerInformation(int StandardTickets, int VIPTickets, String Email){
         this.StandardTickets = StandardTickets;
         this.VIPTickets = VIPTickets;
         this.Email = Email;
     }
     
+    /**
+    * Returns the standard ticket order 
+    * @return standard ticket order
+    */
     public int GetTickets(){
         return this.StandardTickets;
     }
     
+    /** 
+    * Returns the VIP ticket order
+    * @return VIP ticket order
+    */
     public int GetVIPTickets(){
         return this.VIPTickets;
     }
     
+    /** 
+    * Returns customer email
+    * @return customer email
+    */
     public String GetEmail(){
         return this.Email;
     }
     
+    /** 
+    * Returns all customer information
+    * @return all customer information
+    */
     public String toString(){
-        return(Email + "- #OfTickets: " + StandardTickets + " and #OfVIPTickets " + VIPTickets);
+        return(Email + ": #OfTickets: " + StandardTickets + " and #OfVIPTickets " + VIPTickets);
     }
 }
