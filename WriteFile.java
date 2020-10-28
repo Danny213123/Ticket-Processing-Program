0b66b7add71088e16a60681d2b2ec5c8d283007b
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2020-10-28 11:48:28 -0400

    Update WriteFile.java

--- WriteFile.java
+++ WriteFile.java
@@ -1,3 +1,12 @@
+/** 
+ * Writes to output file
+ *
+ * @WriteFile Class
+ * @Author - Danny Guan
+ * @Version - 1
+ *
+ */
+
 import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
@@ -12,16 +21,20 @@ public class WriteFile {
             int MaxStandardTickets = 30;
             int MaxVIPTickets = 10;
 
+            // Scans through the customer info queue
             for (int x = 0; x < CustomerInfo.length; x++){
                 int CustomerVIPTickets = CustomerInfo[x].GetVIPTickets();
                 int CustomerTickets = CustomerInfo[x].GetTickets();
 
+                // If there is enough standard and vip tickets left, order is fulfilled
                 if (MaxVIPTickets - CustomerVIPTickets > 0 & MaxStandardTickets - CustomerTickets > 0){
-                  pw.println(CustomerInfo[x].GetEmail() + "\nYour ticket order has been fulfilled, send us your email and password\nwhich we will send them to our secure payment system to finish their transaction.\n");
-                  MaxVIPTickets -= CustomerVIPTickets;
-                  MaxStandardTickets -= CustomerTickets;
+                    pw.println(CustomerInfo[x].GetEmail() + "\nYour ticket order has been fulfilled, send us your email and password\nwhich we will send them to our secure payment system to finish their transaction.\n");
+                    MaxVIPTickets -= CustomerVIPTickets;
+                    MaxStandardTickets -= CustomerTickets;
+
+                // ticket order cannot be fulfilled
                 } else {
-                     pw.println(CustomerInfo[x].GetEmail() + "\nYour ticket order has not been fulfilled\n");
+                    pw.println(CustomerInfo[x].GetEmail() + "\nYour ticket order has not been fulfilled\n");
                 }
 
                 pw.println(MaxVIPTickets + " VIP tickets left and " + MaxStandardTickets + " MaxStandardTickets left.\n");
@@ -29,20 +42,26 @@ public class WriteFile {
             
             pw.close();
         } catch (Exception e){
-            System.out.println("l");
+            System.out.println("Error");
         }
     }
 
+    /**
+	* Randomly orders the array
+	* @Param CustomerInformation[] Array - Customer index
+	* @updates Customer 2d array with sorted indexes
+	*/
     public CustomerInformation[] RandomizeOrder(CustomerInformation[] Array) {
       Random random = new Random();
 
-      for (int i = Array.length - 1; i > 0; i--) {
-          int Index = random.nextInt(i + 1);
+        // Sorts Array
+        for (int i = Array.length - 1; i > 0; i--) {
+            int Index = random.nextInt(i + 1);
 
-          CustomerInformation Switch = Array[i];
-          Array[i] = Array[Index];
-          Array[Index] = Switch;
-      }
-      return Array;
+            CustomerInformation Switch = Array[i];
+            Array[i] = Array[Index];
+            Array[Index] = Switch;
+        }
+        return Array;
   }
 }
