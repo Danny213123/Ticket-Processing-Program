943f976945cd3aad78514538fbe7cb536f21c74a
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2020-11-02 09:32:31 -0500

    Update WriteFile.java

--- WriteFile.java
+++ WriteFile.java
@@ -8,7 +8,6 @@
  */
 
 import java.io.*;
-import java.util.Random;
 import java.util.Scanner;
 
 public class WriteFile {
@@ -23,9 +22,9 @@ public class WriteFile {
 
             File file = new File("Output.txt");
 
-            FileWriter fw = new FileWriter(file);
+            FileWriter FileWrite = new FileWriter(file);
 
-            PrintWriter pw = new PrintWriter(fw);
+            PrintWriter output = new PrintWriter(FileWrite);
 
             int MaxStandardTickets = 30;
 
@@ -38,44 +37,27 @@ public class WriteFile {
 
                 int CustomerTickets = CustomerInfo[x].GetTickets();
 
-                pw.println("<<EMAIL>>");
+                output.println("<<EMAIL>>");
 
                 // If there is enough standard and vip tickets left, order is fulfilled
                 if (MaxVIPTickets - CustomerVIPTickets > 0 & MaxStandardTickets - CustomerTickets > 0){
-                    pw.println(CustomerInfo[x].GetEmail() + "\nYour ticket order has been fulfilled, send us your email and password\nwhich we will send them to our secure payment system to finish their transaction.");
+                    output.println(CustomerInfo[x].GetEmail() + "\nYour ticket order has been fulfilled, send us your email and password\nwhich we will send them to our secure payment system to finish their transaction.");
                     MaxVIPTickets -= CustomerVIPTickets;
                     MaxStandardTickets -= CustomerTickets;
 
                 // ticket order cannot be fulfilled
                 } else {
-                    pw.println(CustomerInfo[x].GetEmail() + "\nYour ticket order has not been fulfilled");
+                    output.println(CustomerInfo[x].GetEmail() + "\nYour ticket order has not been fulfilled");
                 }
 
-                pw.println("<<END EMAIL>>");
-                //pw.println(MaxVIPTickets + " VIP tickets left and " + MaxStandardTickets + " MaxStandardTickets left.\n");
+                output.println("<<END EMAIL>>");
+                //output.println(MaxVIPTickets + " VIP tickets left and " + MaxStandardTickets + " MaxStandardTickets left.\n");
             }
-            pw.close();
+            output.close();
         } catch (Exception e){
             System.out.println("Error");
         }
     }
 
-    /**
-	* Randomly orders the array
-	* @Param CustomerInformation[] Array - Customer index
-	* @updates Customer 2d array with sorted indexes
-	*/
-    public CustomerInformation[] RandomizeOrder(CustomerInformation[] Array) {
 
-      Random random = new Random();
-
-        // Sorts Array
-        for (int i = Array.length - 1; i > 0; i--) {
-            int Index = random.nextInt(i + 1);
-            CustomerInformation Switch = Array[i];
-            Array[i] = Array[Index];
-            Array[Index] = Switch;
-        }
-        return Array;
-    }
 }
