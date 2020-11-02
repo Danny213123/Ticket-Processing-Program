26b2566263faacf00deff0f64ae918649bd5c480
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2020-11-02 08:40:34 -0500

    Create Lottery.java

---
+++ Lottery.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Lottery{
+
+/**
+* Randomly orders the array
+* @Param CustomerInformation[] Array - Customer index
+* @updates Customer 2d array with sorted indexes
+*/
+
+    public CustomerInformation[] RandomizeOrder(CustomerInformation[] Array) {
+
+        Random random = new Random();
+
+        // Sorts Array
+        for (int i = Array.length - 1; i > 0; i--) {
+            int Index = random.nextInt(i + 1);
+            CustomerInformation Switch = Array[i];
+            Array[i] = Array[Index];
+            Array[Index] = Switch;
+        }
+        return Array;
+    }
+
+}
