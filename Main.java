f979e3e89d844f8264206ef3684cd8f31d54a315
Danny213123 <73540329+Danny213123@users.noreply.github.com>
2020-11-02 20:48:36 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,45 +1,42 @@
-/** 
- * Read, organize and output customer orders
+/** Read, organize and output customer orders
  *
  * Main Class
- * @Author - Danny Guan
- * @Version - 6
- *
+ * @author Danny Guan
+ * @version 6
  */
-
-import java.io.*;
-import java.util.Scanner;
-
 public class Main
 {
     public static void main(String[] args)
     {
-        int Inputs = 3;
+        int inputs = 3;
 
-        ReadFile Scan = new ReadFile(Inputs, 1);
+        ReadFile scan = new ReadFile(inputs, 1);
         
         // Reads input file (Values.txt)
-        String[][] StringArray = Scan.ScanFile();
+        String[][] stringArray = scan.ScanFile();
 
-        Lottery CustomerLottery = new Lottery();
+        Lottery customerLottery = new Lottery();
         
-        CustomerInformation[] Customers = new CustomerInformation[StringArray.length];
+        CustomerInformation[] customers = new CustomerInformation[stringArray.length];
         
         // Creates User objects with correct info
-        for (int y = 0; y < Customers.length; y ++){
-            Customers[y] = new CustomerInformation(Integer.parseInt(StringArray[y][1]),Integer.parseInt(StringArray[y][2]), StringArray[y][0]);
+        for (int y = 0; y < customers.length; y++){
+            customers[y] = new CustomerInformation(Integer.parseInt(stringArray[y][1]),Integer.parseInt(stringArray[y][2]), stringArray[y][0]);
         }
         
-        Customers = CustomerLottery.RandomizeOrder(Customers);
+        customers = customerLottery.RandomizeOrder(customers);
 
         // Outputs file to Output.txt
-        WriteFile Output = new WriteFile();
-        Output.OutputFile(Customers);
+        WriteFile output = new WriteFile();
+        output.OutputFile(customers);
 
         // Prints out user information
-        for (int x = 0; x < Customers.length; x ++){
-          System.out.println(Customers[x].toString() + "\n");
+        System.out.println("Ticket Requests in randomized order....\n");
+
+        for (int x = 0; x < customers.length; x ++){
+          System.out.println(customers[x].toString() + "\n");
         }
-        
+
+        System.out.println("Refer to the Output.txt file to check the email text(s) generated, and if the request(s) could be fulfilled");
     }
 }
